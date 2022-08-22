ce8ef29a6266b3a1b56c829070fe54c3c1df34c1
kot2d <kot2d@outlook.com>
2022-08-23 00:10:46 +0300

    добавил методы для генерация массивов  и вывода массивов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] generateArray(int arraySize)
+{
+    string[] templateArray = new[] { "Тыква", "Сыр", "Кот", "Земля", "Диван", "Мир", ";-)", "Печь", "Нос"};
+    Console.WriteLine("Генерация массива...");
+    int[] numbers = new int[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        numbers[i] = templateArray[new Random().Next(0, templateArray.GetLength-1)];
+    }
+    Console.WriteLine("Генерация массива закончена");
+    return numbers;
+}
+
+void printArray(int[] array)
+{
+    Console.WriteLine("Печать массива: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        string divider = i < array.GetLength - 1 ? " ," : "\n";
+        Console.WriteLine(array[i]);
+    }
+}
+
