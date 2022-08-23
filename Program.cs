225f7a622d86620d9e4a6ca174f2c6f0f0d113be
kot2d <kot2d@outlook.com>
2022-08-23 18:09:47 +0300

    исправил методы generateArray и printArray

--- Program.cs
+++ Program.cs
@@ -2,22 +2,25 @@
 {
     string[] templateArray = new[] { "Тыква", "Сыр", "Кот", "Земля", "Диван", "Мир", ";-)", "Печь", "Нос"};
     Console.WriteLine("Генерация массива...");
-    int[] numbers = new int[arraySize];
+    string[] numbers = new string[arraySize];
     for (int i = 0; i < arraySize; i++)
     {
-        numbers[i] = templateArray[new Random().Next(0, templateArray.GetLength-1)];
+        int index = new Random().Next(0, templateArray.Length - 1);
+        numbers[i] = templateArray[index];
     }
     Console.WriteLine("Генерация массива закончена");
     return numbers;
 }
 
-void printArray(int[] array)
+void printArray(string[] array)
 {
     Console.WriteLine("Печать массива: ");
     for (int i = 0; i < array.Length; i++)
     {
-        string divider = i < array.GetLength - 1 ? " ," : "\n";
-        Console.WriteLine(array[i]);
+        string divider = i < array.Length - 1 ? "," : "\n";
+        Console.WriteLine(array[i] + divider);
     }
 }
 
+string[] temp = generateArray(8);
+printArray(temp);
